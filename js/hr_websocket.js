7bd1846c64f66f3ffc3ebce0b03314b4607afb77
Calvin Tee <calvin@alphamelon.com>
2021-04-25 03:12:28 +0800

    do not spawn too many websocket

--- js/hr_websocket.js
+++ js/hr_websocket.js
@@ -1,24 +1,26 @@
 class HeartSocket {
+  websocket = ''
   constructor(wsUri, connectedCallback, disconnectedCallback, dataCallback)
   {
-    this.connectWs(wsUri, connectedCallback, disconnectedCallback, dataCallback)
+    this.websocket = new WebSocket(wsUri);
+    this.connectWs(connectedCallback, disconnectedCallback, dataCallback)
   }
-  connectWs(wsUri, connectedCallback, disconnectedCallback, dataCallback)
+  connectWs(connectedCallback, disconnectedCallback, dataCallback)
   {
     const vm = this
-    var websocket = new WebSocket(wsUri);
+    const websocket = this.websocket
     websocket.onopen = connectedCallback
     websocket.onclose = function(evt) {
       disconnectedCallback(evt)
       setTimeout(function(){
-        vm.connectWs(wsUri, connectedCallback,disconnectedCallback, dataCallback)
+        vm.connectWs(connectedCallback,disconnectedCallback, dataCallback)
       }, 5000)
     };
     websocket.onmessage = dataCallback;
     websocket.onerror = function(evt) { 
       disconnectedCallback(evt)
       setTimeout(function(){
-        vm.connectWs(wsUri, connectedCallback,disconnectedCallback, dataCallback)
+        vm.connectWs(connectedCallback,disconnectedCallback, dataCallback)
       }, 5000)
     };
   }
