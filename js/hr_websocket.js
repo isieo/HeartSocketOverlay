1aaa17e869229e9f216c7ab21d7eee003456a6c1
Calvin Tee <calvin@alphamelon.com>
2021-04-26 00:37:41 +0800

    beatsaber overlay

--- js/hr_websocket.js
+++ js/hr_websocket.js
@@ -1,26 +1,31 @@
 class HeartSocket {
-  websocket = ''
   constructor(wsUri, connectedCallback, disconnectedCallback, dataCallback)
   {
-    this.websocket = new WebSocket(wsUri);
-    this.connectWs(connectedCallback, disconnectedCallback, dataCallback)
+    this.connectWs(wsUri, connectedCallback, disconnectedCallback, dataCallback)
   }
-  connectWs(connectedCallback, disconnectedCallback, dataCallback)
+  connectWs(wsUri, connectedCallback, disconnectedCallback, dataCallback)
   {
     const vm = this
-    const websocket = this.websocket
+    var reconnectionTimeout = 0
+    var websocket = new WebSocket(wsUri);
     websocket.onopen = connectedCallback
     websocket.onclose = function(evt) {
       disconnectedCallback(evt)
-      setTimeout(function(){
-        vm.connectWs(connectedCallback,disconnectedCallback, dataCallback)
+      console.log('Connection Closed')
+      clearTimeout(reconnectionTimeout)
+      reconnectionTimeout = setTimeout(function(){
+        websocket.close()
+        vm.connectWs(wsUri, connectedCallback,disconnectedCallback, dataCallback)
       }, 5000)
     };
     websocket.onmessage = dataCallback;
     websocket.onerror = function(evt) { 
       disconnectedCallback(evt)
-      setTimeout(function(){
-        vm.connectWs(connectedCallback,disconnectedCallback, dataCallback)
+      console.log('Connection Error')
+      clearTimeout(reconnectionTimeout)
+      reconnectionTimeout = setTimeout(function(){
+        websocket.close()
+        vm.connectWs(wsUri, connectedCallback,disconnectedCallback, dataCallback)
       }, 5000)
     };
   }
